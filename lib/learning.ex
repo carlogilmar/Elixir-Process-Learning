d9a717502acda50373f7428241b08cf8dbed40a0
Carlo Gilmar <carlogilmar12@gmail.com>
2018-04-01 00:55:16 -0600

    Adding second example of how to create a simple process with spawn

--- lib/learning.ex
+++ lib/learning.ex
@@ -6,4 +6,13 @@ defmodule Learning do
     loop( number + 1 )
   end
 
+  def lonely_loop(n\\0) do
+    IO.inspect n
+    Process.sleep 1_000
+    lonely_loop(n+1)
+  end
+
+  #IO.inspect spawn(&Learning.lonely_loop/0)
+  #PID<0.123.0>
+
 end
