84469096758b0cb210669d39de2c2e05b648468d
Carlo Gilmar <carlogilmar12@gmail.com>
2018-04-01 01:40:38 -0600

    Adding simple example creating a process for update a list

---
+++ lib/storage.ex
@@ -0,0 +1,28 @@
+defmodule Storage do
+	def start(initial) do
+		process = spawn_link(fn -> execute(initial) end)
+		Process.register(process, Storage)
+		process
+	end
+
+	def execute(state) do
+		receive do
+			{:get, sender} ->
+				send(sender, state)
+				execute(state)
+			{:put, val} ->
+				execute(state ++ [val])
+		end
+	end
+
+	def put(value) do
+		send(Storage, {:put, value})
+	end
+
+	def get do
+		send(Storage, {:get, self()})
+		receive do
+			msg -> msg
+		end
+	end
+end
