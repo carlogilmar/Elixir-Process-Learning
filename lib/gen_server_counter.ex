72ae30e7182c593f885a5bf8c870d0d1cfb7c085
Carlo Gilmar <carlogilmar12@gmail.com>
2018-04-02 01:20:38 -0500

    Adding my finally gen server simple example

--- lib/gen_server_counter.ex
+++ lib/gen_server_counter.ex
@@ -1,5 +1,7 @@
 defmodule SimpleGenserver do
 
+  # POST https://medium.com/learn-elixir/genserver-explained-in-5-minutes-ae5823af4d44
+
   require Logger
   use GenServer
 
@@ -11,10 +13,21 @@ defmodule SimpleGenserver do
     {:ok, 1}
   end
 
-  def handle_call do
+  def handle_call(:get_data, _, state) do
+    # this runs in the server context
+    {:reply, state, state}
   end
 
-  def handle_cast do
+  def handle_cast(:increment, state) do
+    {:noreply, state + 1}
   end
 
 end
+
+#iex(1)> pid = SimpleGenServerMock.start_link
+##PID<0.128.0>
+#iex(3)> counter = SimpleGenServerMock.call(pid, :get_data)
+#1
+#iex(4)> SimpleGenServerMock.cast(pid, :increment)
+#iex(5)> counter = SimpleGenServerMock.call(pid, :get_data)
+#2
